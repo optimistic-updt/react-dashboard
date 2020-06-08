848c11239531b90c00bcd71c0642465a82f69075
Kevi G <kevgarciamusic@hotmail.com>
2020-06-08 18:30:09 +1000

    starting sketching, did nav bar and first component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import './grid.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main-header section group">
+        <img src="../logo192.png" alt="logo" className="logo"/>
+        
+        <nav className="nav">
+          <a href="#">Overview</a>
+          <a href="#">Calendar</a>
+          <a href="#">Finance</a>
+          <a href="#">Fitness</a>
+        </nav>
+        <button className="button button-user">User</button>
       </header>
+
+      <main>
+        <section className="section section--feeling span_2_of_4">
+          <input type="text" placeholder="How are you feeling today?"/>
+          <output>This is a test script</output>
+          <button className="button button-analyse">Analyze</button>
+        </section>
+
+        <section className="section section--calendar"></section>
+        <section className="section--weather"></section>
+        <section className="section--news"></section>
+        <section className="section--sleeptracker"></section>
+        <section className="section--todo"></section>
+        <section></section>
+      </main>
+
+      <footer className="main-footer">
+        <p>&copy; Kevin Garcia Fernandez</p>
+        <i>GitHub</i>
+        <i>GitHub</i>
+        <i>GitHub</i>
+        <i>GitHub</i>
+      </footer>
+
     </div>
   );
 }
