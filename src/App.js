d5478a299e4d4c9aba30eeee51c8d22dd6c2414e
Kevi G <kevgarciamusic@hotmail.com>
2020-06-12 09:36:10 +1000

    added hide button for dark mode impact

--- src/App.js
+++ src/App.js
@@ -16,36 +16,54 @@ import FooterMain from './Footer_main_component/Footer_main'
 const dotenv = require('dotenv');
 dotenv.config();
 
+class Chart extends React.Component {
+  state = {
+    isHidden: false
+  }
 
+  toggleView = () => {
+    this.setState({
+      isHidden: this.state.isHidden ? false : true
+    })
+  }
+
+  render() {
+    let style = { display: this.state.isHidden ? 'none' : 'inline'}
+
+    return (
+      <div className="widget chart">
+      <img src="https://docs.moodle.org/dev/images_dev/c/c5/bar_chart.png" alt="chart" style={style} />
+      <button onClick={this.toggleView}>Hide</button>
+    </div>
+    )
+  }
+}
 
 
 function App() {
-    const [theme, toggleTheme] = useDarkMode()
+  const [theme, toggleTheme] = useDarkMode()
 
-    return (
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-        <>
-          <GlobalStyles />
-            <div className="App">
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+          <div className="App">
 
-              <HeaderMain theme={theme} toggleTheme={toggleTheme}/>
+            <HeaderMain theme={theme} toggleTheme={toggleTheme}/>
 
-              <main className="main-wrapper">
-                <Sentiment />
-                <Weather />
-                <ToDo />
-                <div className="widget chart">
-                  <img src="https://docs.moodle.org/dev/images_dev/c/c5/bar_chart.png" alt="chart" />
-                </div>
-                <div className="widget news">news</div>
-              </main>
+            <main className="main-wrapper">
+              <Sentiment />
+              <Weather />
+              <ToDo />
+              <Chart />
+              <div className="widget news">news</div>
+            </main>
 
-              <FooterMain />
-            </div>
-        </>
-      </ThemeProvider>
-    );
-
+            <FooterMain />
+          </div>
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
